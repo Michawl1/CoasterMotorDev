8826e389b7b6e82536f7a7afa2e6aed6460b38fa
Michael Thompson <mjt106@case.edu>
2019-07-26 22:09:28 -0700

    Base code working, got motors spinning

---
+++ src/main/main.ino
@@ -0,0 +1,23 @@
+/*******************************************************************************
+ * @file Main.ino
+ * @author Michael Thompson
+ * @brief This file outlines the main application of the coaster motor dev
+ *        project
+ * @details
+*******************************************************************************/
+#include <Adafruit_PWMServoDriver.h>
+
+Adafruit_PWMServoDriver g_motorDriver = Adafruit_PWMServoDriver();
+
+void setup() {
+  g_motorDriver.begin();
+  g_motorDriver.setPWMFreq(50000);
+}
+
+void loop() {
+  g_motorDriver.setPWM(0, 0, 50);
+  delay(1000);
+  g_motorDriver.setPWM(0, 0, 250);
+  delay(1000);
+
+}
