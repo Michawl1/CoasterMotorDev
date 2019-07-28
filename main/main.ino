494af28cb5bd13050588c9dbb0cbe11e6a398359
Michael Thompson <mjt106@case.edu>
2019-07-28 11:55:36 -0700

    This should work, need to alter numbers when I build it

--- main/main.ino
+++ main/main.ino
@@ -13,46 +13,42 @@ Adafruit_PWMServoDriver g_motorDriver = Adafruit_PWMServoDriver();
 uint8_t g_interrupt1 = 2;
 uint8_t g_interrupt2 = 3;
 uint8_t g_state = 0;
+uint8_t val = 0;
 
 void setup()
 {
   g_motorDriver.begin();
   g_motorDriver.setPWMFreq(50000);
 
-  pinMode(g_interrupt1, INPUT_PULLUP);
-  pinMode(g_interrupt2, INPUT_PULLUP);
-
-  //This is attached to uno R3 pin 2
-  attachInterrupt(INT0, interrupt1, RISING);
-  //This is attached to uno R3 pin 3
-  attachInterrupt(INT1, interrupt2, RISING);
+  pinMode(g_interrupt1, INPUT);
+  pinMode(g_interrupt2, INPUT);
 }
 
 void loop()
 {
+  //brakes sequence
+  val = digitalRead(g_interrupt1);
+  if(val != LOW)
+  {
+    delay(4000);
+    g_motorDriver.setPWM(0, 0, 50);
+    delay(1500);
+    g_motorDriver.setPWM(0, 0, 250);
+  }
 
-}
+  //station sequence
+  val = digitalRead(g_interrupt2);
+  if(val != LOW)
+  {
+    delay(1000);
+    g_motorDriver.setPWM(1, 0, 50);
+    delay(2500);
+    g_motorDriver.setPWM(1, 0, 250);
+    delay(1000);
 
-//Switch tied to brakes
-void interrupt1()
-{
-  delay(1000);
-  g_motorDriver.setPWM(0, 0, 50);
-  delay(1000);
-  g_motorDriver.setPWM(0, 0, 250);
-}
-
-//Switch tied to station
-void interrupt2()
-{
-  g_motorDriver.setPWM(1, 0, 50);
-  delay(1000);
-  g_motorDriver.setPWM(1, 0, 250);
-  delay(1000);
-  
-  //deploy motor
-  g_motorDriver.setPWM(2, 0, 50);
-  delay(1000);
-  g_motorDriver.setPWM(2, 0, 250);
-  delay(1000);
+    g_motorDriver.setPWM(2, 0, 50);
+    delay(2000);
+    g_motorDriver.setPWM(2, 0, 250);
+    delay(1000);
+  }
 }
