2d36a1402817aef3dad17ccc20362288afc5e82a
Michael Thompson <mjt106@case.edu>
2019-09-05 15:21:04 -0400

    Adding the used code for the last coaster

--- main/main.ino
+++ main/main.ino
@@ -10,7 +10,7 @@
 #include <Wire.h>
 
 Adafruit_PWMServoDriver g_motorDriver = Adafruit_PWMServoDriver();
-uint8_t g_interrupt1 = 2;
+//uint8_t g_interrupt1 = 2;
 uint8_t g_interrupt2 = 3;
 uint8_t g_state = 0;
 uint8_t val = 0;
@@ -20,35 +20,33 @@ void setup()
   g_motorDriver.begin();
   g_motorDriver.setPWMFreq(50000);
 
-  pinMode(g_interrupt1, INPUT);
+  //pinMode(g_interrupt1, INPUT);
   pinMode(g_interrupt2, INPUT);
 }
 
 void loop()
 {
   //brakes sequence
-  val = digitalRead(g_interrupt1);
-  if(val != LOW)
+  val = digitalRead(g_interrupt2);
+  //if(val != LOW)
+  while(true)
   {
     delay(4000);
-    g_motorDriver.setPWM(0, 0, 50);
-    delay(1500);
-    g_motorDriver.setPWM(0, 0, 250);
-  }
-
-  //station sequence
-  val = digitalRead(g_interrupt2);
-  if(val != LOW)
-  {
-    delay(1000);
-    g_motorDriver.setPWM(1, 0, 50);
-    delay(2500);
-    g_motorDriver.setPWM(1, 0, 250);
-    delay(1000);
-
-    g_motorDriver.setPWM(2, 0, 50);
+    g_motorDriver.setPWM(0, 0, 68);
+    delay(4000);
+    g_motorDriver.setPWM(0, 0, 94);
+  
     delay(2000);
-    g_motorDriver.setPWM(2, 0, 250);
-    delay(1000);
+    g_motorDriver.setPWM(1, 0, 90);
+    delay(2000);
+    g_motorDriver.setPWM(1, 0, 220);
+
+    delay(2000);
+    g_motorDriver.setPWM(2, 0, 50);
+    delay(5000);
+    g_motorDriver.setPWM(2, 0, 92);
+
+    delay(32000);
   }
+  
 }
